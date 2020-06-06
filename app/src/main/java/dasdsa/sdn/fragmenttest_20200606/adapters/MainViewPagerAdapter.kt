058eeb6b06483954db2e0d4dbe25f12fd5b536a7
umgonji <umgonji@naver.com>
2020-06-06 16:03:19 +0900

    페이지별 제목 추가

--- app/src/main/java/dasdsa/sdn/fragmenttest_20200606/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/dasdsa/sdn/fragmenttest_20200606/adapters/MainViewPagerAdapter.kt
@@ -7,6 +7,16 @@ import dasdsa.sdn.fragmenttest_20200606.fragments.MyProfileFragment
 import dasdsa.sdn.fragmenttest_20200606.fragments.StudentListFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if( position == 0 ) {
+            return "내 프로필"
+        }
+        else {
+            return "수강생 목록"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
         if( position == 0 ) {
             return MyProfileFragment();
