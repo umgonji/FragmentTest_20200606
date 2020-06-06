f1201e21f78a2249412d01fa2b8c86db586c022b
umgonji <umgonji@naver.com>
2020-06-06 14:36:18 +0900

    연결버튼 누르면 저노하가 걸리도록  => Fragment에서 액티비티의 코드를 짜듯이 표현

--- app/src/main/java/dasdsa/sdn/fragmenttest_20200606/fragments/MyProfileFragment.kt
+++ app/src/main/java/dasdsa/sdn/fragmenttest_20200606/fragments/MyProfileFragment.kt
@@ -1,11 +1,15 @@
 package dasdsa.sdn.fragmenttest_20200606.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dasdsa.sdn.fragmenttest_20200606.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 import kotlinx.android.synthetic.main.fragment_myprofile.*
 
 class MyProfileFragment : Fragment() {
@@ -20,6 +24,15 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        dialBtn.setOnClickListener {
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:$inputPhoneNum")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+
+        }
     }
 
 }
